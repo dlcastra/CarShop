d956399b939e932c5bff7f57697dbcc7b1b99805
dlcastra <1dlcastra@gmail.com>
2023-10-22 19:28:48 +0200

    Added view for registering and displaying the list of cars

--- carshop/store/views.py
+++ carshop/store/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+from store.forms import ClientForm
+from store.models import Car
+
+
+def register_client(request):
+    if request.method == "GET":
+        form = ClientForm
+        return render(request, "add_client.html", {"client": form})
+
+    form = ClientForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect("redirect_on_store_page")
+
+    return render(request, "add_client.html", {"client": form})
+
+
+def redirect_on_store_page(request):
+    car_list = Car.objects.all()
+    return render(request, "store_page.html", {"cars": car_list})
