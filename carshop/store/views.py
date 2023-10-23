7d779b2966d968bff24306ece58ecee93be51800
dlcastra <1dlcastra@gmail.com>
2023-10-23 16:54:03 +0200

    created functions for creating and displaying car types

--- carshop/store/views.py
+++ carshop/store/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect
 
-from store.forms import ClientForm
-from store.models import Car
+from store.forms import ClientForm, CarTypeForm, CarForm
+from store.models import Car, CarType
+
+""" --- CLIENT PART --- """
 
 
 def register_client(request):
@@ -20,3 +22,28 @@ def register_client(request):
 def redirect_on_store_page(request):
     car_list = Car.objects.all()
     return render(request, "store_page.html", {"cars": car_list})
+
+
+""" --- STAFF PART --- """
+
+
+def get_all_types_of_cars(request):
+    car_type_list = CarType.objects.all()
+    return render(request, "all_types_of_cars.html", {"car_type": car_type_list})
+
+
+def add_new_car_type(request):
+    if request.method == "GET":
+        form = CarTypeForm
+        return render(request, "add_car_type.html", {"car_type": form})
+
+    form = CarTypeForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect("get_all_types_of_cars")
+
+    return render(request, "add_car_type.html", {"car_type": form})
+
+
+def add_new_car(request):
+    ...
