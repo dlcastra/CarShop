8ed3a47144a1a3623dbe56e93cc24bc74714770a
dlcastra <1dlcastra@gmail.com>
2023-10-23 16:54:40 +0200

    created forms for models Car and CarType

--- carshop/store/forms.py
+++ carshop/store/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 import phonenumbers
 
-from store.models import Client
+from store.models import Client, CarType, Car
 
 
 class ClientForm(forms.ModelForm):
@@ -36,10 +36,6 @@ class ClientForm(forms.ModelForm):
                 "The e-mail address is too long/Адреса електронної пошти занадто довга"
             )
 
-        # if "@" in email:
-        #     raise forms.ValidationError(
-        #         "The email address cannot be without an @ sign/Адреса електронної пошти не може бути без знаку @"
-        #     )
         return email
 
     def clean_phone(self):
@@ -65,3 +61,67 @@ class ClientForm(forms.ModelForm):
             parsed, phonenumbers.PhoneNumberFormat.INTERNATIONAL
         )
         return formatted_phone
+
+
+class CarTypeForm(forms.ModelForm):
+    class Meta:
+        model = CarType
+        fields = ["name", "brand", "price"]
+        labels = {"name": "Назва авто", "brand": "Марка авто", "price": "Ціна"}
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        if len(name) > 50:
+            raise forms.ValidationError(
+                "Name is too long, max length is 50/Назва занадто вилекa, максимальна довжина 50"
+            )
+        return name
+
+    def clean_brand(self):
+        brand = self.cleaned_data["brand"]
+        if len(brand) > 50:
+            raise forms.ValidationError(
+                "Brand name is too long, max length is 50/Назва бренду занадто вилекa, максимальна довжина 50"
+            )
+        return brand
+
+    # def clean_price(self):
+    #     price = self.cleaned_data["price"]
+    #     if any(char.isalpha() for char in price):
+    #         raise forms.ValidationError(
+    #             "The price should not have a letter /Ціна не повинна мати літери"
+    #         )
+    #     return price
+
+
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = ["car_type", "color", "year"]
+        labels = {"car_type": "Тип авто", "color": "Колір", "year": "Рік"}
+
+    def clean_color(self):
+        color = self.cleaned_data["color"]
+        if len(color) > 50:
+            raise forms.ValidationError(
+                "Color name is too long, max length is 50/Назва кольору занадто вилекa, максимальна довжина 50"
+            )
+
+        if any(char.isdigit() for char in color):
+            raise forms.ValidationError(
+                "The color name must not contain any numbers/Назва кольору не повинно мати цифр"
+            )
+        return color
+
+    def clean_year(self):
+        year = self.cleaned_data["year"]
+        if len(year) < 4:
+            raise forms.ValidationError(
+                "Specify the full year of manufacture of the car/Вкажіть повний рік випуску авто "
+            )
+
+        if any(char.isalpha() for char in year):
+            raise forms.ValidationError(
+                "The year should not have a letter /Рік випуску не повинен мати літери"
+            )
+        return year
