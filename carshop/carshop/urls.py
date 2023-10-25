73a0463da59c595dd81fb7e26cca00bdc3db8d0d
dlcastra <1dlcastra@gmail.com>
2023-10-25 18:43:01 +0200

    removed unnecessary url

--- carshop/carshop/urls.py
+++ carshop/carshop/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),
-    path("for-staff-only/", include("store.urls")),
-    path("add-new-car-type/", include("store.urls")),
 ]
