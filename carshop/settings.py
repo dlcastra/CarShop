8d0f36c5dbfb4e88ef7f1df4dbfbb48e28f7a3b0
dlcastra <1dlcastra@gmail.com>
2023-12-23 13:44:54 +0100

    Added base config for API

--- carshop/settings.py
+++ carshop/settings.py
@@ -235,7 +235,7 @@ SOCIALACCOUNT_PROVIDERS = {
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
     ]
 }
