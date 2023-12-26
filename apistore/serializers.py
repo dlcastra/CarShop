902c284b6b1072143cbefe957f21f5df67524dc2
dlcastra <1dlcastra@gmail.com>
2023-12-26 17:57:00 +0100

    Added GET methods

--- apistore/serializers.py
+++ apistore/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from store.models import Car
+from store.models import Car, CarType, Dealership
+
+
+class CarTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarType
+        fields = ["name", "brand", "price"]
+
+
+class DealershipSerializer(serializers.ModelSerializer):
+    available_car_types = CarTypeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Dealership
+        fields = ["name", "available_car_types"]
 
 
 class CarSerializer(serializers.ModelSerializer):
+    car_type = CarTypeSerializer(read_only=True)
+
     class Meta:
         model = Car
-        fields = "__all__"
+        fields = ["id", "color", "year", "image", "car_type"]
--- apistore/views.py
+++ apistore/views.py
@@ -1,18 +1,40 @@
-from rest_framework.views import APIView
+# from django.http import JsonResponse
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-from store.models import Car
+from store.models import Car, Dealership, CarType
 
-from apistore.serializers import CarSerializer
+from apistore.serializers import CarSerializer, DealershipSerializer, CarTypeSerializer
 
 
-class ApiCars(APIView):
-    http_method_names = ["get", "post", "put"]
+# GET AND POST METHODS
+
+
+class CarTypeViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get"]
+    queryset = CarType.objects.all()
+    serializer_class = CarTypeSerializer
 
     def get(self, request):
-        ...
+        return Response(self.serializer_class.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        ...
 
-    def put(self, request):
-        ...
+class CarViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get"]
+    queryset = Car.objects.filter(owner__isnull=True, blocked_by_order__isnull=True)
+    serializer_class = CarSerializer
+
+    def get(self, request):
+        return Response(self.serializer_class.data, status=status.HTTP_200_OK)
+
+
+class DealersViewSet(viewsets.ModelViewSet):
+    http_method_names = ["get"]
+    queryset = Dealership.objects.all()
+    serializer_class = DealershipSerializer
+
+    def get(self, request):
+        return Response(self.serializer_class.data, status=status.HTTP_200_OK)
+
+
+# PUT METHODS
